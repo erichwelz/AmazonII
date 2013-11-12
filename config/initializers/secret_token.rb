c8f54f03f0c8c734130fe6844c27772259cb167f
Erich Welz <erichwelz@gmail.com>
2013-11-12 15:11:16 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GiantRiver::Application.config.secret_key_base = '691e310d99e6bb8d2d52a2763f77f2205c1e1ac038fd45dea57f2fc3900e84f5e8cd096804b56dac8cf9845fd77de6adf7de457cf58b8ae98ecd6f2f41c68708'
